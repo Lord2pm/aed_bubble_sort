a3166769bdc0eb437fe68c709c4dc9b49093cb83
lord2pm <luismuhele@gmail.com>
2025-08-11 08:18:52 +0100

    fix/ reafoctor the bubble sort algorithm

--- src/routes/teams_routes.py
+++ src/routes/teams_routes.py
@@ -1,57 +1,42 @@
 from flask import Blueprint, render_template
 
 from db.db import DbOperations
+from utils.bubble_sort import bubble_sort
 
 teams_blueprint = Blueprint("teams", __name__, url_prefix="/")
 
 
 @teams_blueprint.route("/")
-def index():
+def index_view():
     with DbOperations() as db:
         teams = db.get_clubes()
-    print(teams)
     return render_template("index.html", equipas=teams)
 
 
 @teams_blueprint.route("/sobre")
-def sobre():
+def sobre_view():
     return render_template("sobre.html")
 
 
-"""
-@teams_blueprint.route("/ordenar/<id>")
-def ordenar(id):
-    dados = model.get_clubes()
-    lista_nomes = []
-    lista_orcamentos = []
-    lista_titulos = []
+@teams_blueprint.route("/ordenar/<index>")
+def ordenar_view(index):
+    page_titles = {
+        "1": "ordenadas de A - Z",
+        "2": "dispostas pelos seus orçamentos",
+        "3": "dispostas pela quantidade de títulos",
+    }
+    with DbOperations() as db:
+        teams = db.get_clubes()
+    print(teams)
 
-    for dado in dados:
-        lista_nomes.append(dado[1])
-        lista_orcamentos.append(dado[2])
-        lista_titulos.append(dado[3])
-
-    if id == "1":
-        titulo = "ordenadas de A - Z"
-        lista_ordenada = bubble_sort(lista_nomes)
-        campo = "nome"
-    elif id == "2":
-        lista_orcamentos = list(set(lista_orcamentos))
-        lista_ordenada = bubble_sort(lista_orcamentos)
-        titulo = "dispostas pelos seus orçamentos"
-        campo = "orcamento"
-    else:
-        lista_titulos = list(set(lista_titulos))
-        lista_ordenada = bubble_sort(lista_titulos)
-        others = model.get_clube("orcamento", dado)
-        titulo = "dispostas pela quantidade de títulos"
-        campo = "qtd_titulos"
+    titulo = page_titles.get(index)
+    lista_ordenada = bubble_sort(teams, int(index))
+    campo = "nome"
 
     return render_template(
         "ordenar.html",
-        model=model,
+        model=teams,
         lista_ordenada=lista_ordenada,
         titulo=titulo,
         campo=campo,
     )
-"""
--- src/utils/bubble_sort.py
+++ src/utils/bubble_sort.py
@@ -1,13 +1,9 @@
-def bubble_sort(lista):
-	qtd_elementos = len(lista)
-	
-	for i in range(qtd_elementos - 1):
-		for i in range(qtd_elementos - 1):
-			if type(lista[i]) == int:
-				if lista[i+1] > lista[i]:
-					lista[i+1], lista[i] = lista[i], lista[i+1]
-			else:
-				if lista[i+1] < lista[i]:
-					lista[i+1], lista[i] = lista[i], lista[i+1]
-	
-	return lista
+def bubble_sort(lista_equipas: list[tuple], index: int):
+    for i in range(len(lista_equipas) - 1):
+        for j in range(len(lista_equipas) - 1):
+            if lista_equipas[j + 1][index] < lista_equipas[i][index]:
+                lista_equipas[i + 1], lista_equipas[i] = (
+                    lista_equipas[i],
+                    lista_equipas[i + 1],
+                )
+    return lista_equipas
